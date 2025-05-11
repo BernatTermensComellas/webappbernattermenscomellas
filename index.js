c9fd2f87a84006dbfa98992400edaac7fbdc7a79
BernatTermensComellas <bernat.termens@institutroquetes.cat>
2025-05-11 21:15:38 +0200

    login

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-let scriptURL = "https://script.google.com/macros/s/AKfycbwCSO-3s4QaV4DOo-fUiaLh-G54cWF4m-SDY6_dw_SbNxAaHMCcPMh6m8gpsd_MbMpQQw/exec"   // aquí s'ha d'escriure l'URL del script
-let consulta = scriptURL + "?query=select&where=Tipus&is=Turisme";
+function canvia_seccio(num_boto) {
+    const menu = document.getElementById("menu");
+    const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
+    for (let i = 1; i < num_botons; i++) {
+        let boto = document.getElementById("boto_" + i);
+        let seccio = document.getElementById("seccio_" + i);
+        if (i == num_boto) {
+            boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+            boto.style.backgroundColor = "#FCDEE0";
+            seccio.style.display = "flex";    // es fa visible la secció activa
+        }
+        else {
+            boto.style.color = "white";    // colors dels botons de seccions inactives
+            boto.style.backgroundColor = "#950E17";
+            seccio.style.display = "none";    // s'oculten les seccions inactives
+        }
+    }
+}
+let validat = false;    // variable que permet saber si hi ha algun usuari validat
+let nom, contrasenya;
+let scriptURL = "https://script.google.com/macros/s/AKfycbwCSO-3s4QaV4DOo-fUiaLh-G54cWF4m-SDY6_dw_SbNxAaHMCcPMh6m8gpsd_MbMpQQw/exec"    // s'ha de substituir la cadena de text per la URL del script
+
+function inici_sessio() {
+    nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
+    contrasenya = document.getElementById("contrasenya").value;
+    let consulta = scriptURL + "?query=select&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
     fetch(consulta)
-        .then((resposta) => {    // primera "promesa"
-            return resposta.json();    // retorna les dades JSON obtingudes com a llista
+        .then((resposta) => {   // registres que contenen el nom d'usuari i contrasenya escrits per l'usuari
+            return resposta.json();    // conversió a llista
         })
-        .then((resposta) => {    // segona "promesa". Resposta = [{"Tipus":"Turisme","Motor":"Elèctric","Any":"2022","Velocitat":"120"}]
-// espai per a les instruccions que s'executen en rebre la llista
-        });
+        .then((resposta) => {
+            if(resposta.length == 0) {    // llista buida
+                window.alert("El nom d'usuari o la contrasenya no són correctes.");
+            }
+            else {    // llista amb (almenys) un registre
+                window.alert("S'ha iniciat correctament la sessió.");
+                inicia_sessio();    // usuari validat, s'executen les instruccions del procediment "inicia_sessio"
+            }
+        });    
+}
