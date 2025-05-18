88bba886c7acdb50872247666623646a2d91f95e
BernatTermensComellas <bernat.termens@institutroquetes.cat>
2025-05-18 20:13:21 +0200

    BUBGBV

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ function geoExit(posicio){
 }
 
 async function inicia_video() {
-    const codi_model = "JwBUbg1n7"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
+    const codi_model = "RgVOQFau3"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
     const tmURL = "https://teachablemachine.withgoogle.com/models/" + codi_model;
     const modelURL = tmURL + "/model.json";
     const metadataURL = tmURL + "/metadata.json";
